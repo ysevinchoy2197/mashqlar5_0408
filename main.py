4ff3d6181fc86927198b0f522d37cc1af5ae3619
Sevinch <ysevinchoy2197@gmail.com>
2026-04-08 17:20:37 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+#16-misol
+roy = ["10", "20", "30"]
+print(roy)
+
+res = list(map(lambda x: int(x) + 10, roy))
+print(res)
+
+#18-misol
+roy = ["olma", "anor", "uzum"]
+print(roy)
+
+res = list(map(lambda x: x[0], roy))
+print(res)
+
+#19-misol
+roy = [11, 22, 33, 44]
+print(roy)
+
+res = list(map(lambda x: str(x), roy ))
+print(res)
+
+#20-misol
+roy = ["python", "map", "function"]
+print(roy)
+
+res = list(map(lambda x: x * 2, roy))
+print(res)
+
+21-misol
+roy = [0, 1, 2, 3]
+print(roy)
+
+res = list(map(lambda x: x + 10, roy))
+print(res)
+
+#22-misol
+roy = ["x", "y", "z"]
+print(roy)
+
+res = list(map(lambda x: x + 'harf:', roy))
+print(res)
+
+#23-misol
+roy = [50, 100, 150]
+print(roy)
+
+res = list(map(lambda x: x / 2, roy))
+print(res)
+
+#24-misol
+roy = ["5", "6", "7"]
+print(roy)
+
+res = list(map(lambda x: int(x) / 2, roy))
+print(res)
+
+#25-misol
+roy = [8, 9, 10]
+print(roy)
+
+res = list(map(lambda x: x ** 2, roy))
+print(res)
